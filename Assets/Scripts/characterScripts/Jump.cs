1511ef5fbe2eacb6d5796d40e5e8832094b2471e
enriquefbrt <enriquefbrt@gmail.com>
2024-11-29 19:03:22 +0100

    SUPER jump

--- Assets/Scripts/characterScripts/Jump.cs
+++ Assets/Scripts/characterScripts/Jump.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class JumpScript : MonoBehaviour
 {
+    public Animator chrWalk;
     public float jumpHeight = 2.71828f; // Desired jump height
     private Rigidbody rb; // Reference to the Rigidbody component
     private bool isGrounded = true; // Check if the player is grounded
+
+    private float _timeKeyPressed = 0f; // Tiempo acumulado de la tecla presionada
+    private bool _isKeyBeingPressed = false; // Estado de la tecla
 
     void Start()
     {
         // Get the Rigidbody component attached to this GameObject
         rb = GetComponent<Rigidbody>();
+        chrWalk = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -17,14 +22,31 @@ public class JumpScript : MonoBehaviour
         // Check for jump input
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            chrWalk.SetBool("spacebar", true);
+            _isKeyBeingPressed = true;
+            _timeKeyPressed = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && _isKeyBeingPressed)
+        {
+            _timeKeyPressed += Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded)
+        {
+            chrWalk.SetBool("spacebar", false);
+            chrWalk.SetBool("jumping", true);
             Jump();
+            _isKeyBeingPressed = false;
         }
     }
 
     void Jump()
     {
         // Calculate the required jump force to reach the desired height
-        float jumpForce = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpHeight);
+        float jumpForce;
+        if (_timeKeyPressed < 0.75f) { jumpForce = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpHeight); }
+        else { jumpForce = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * 1.75f * jumpHeight); }
 
         // Apply the jump force directly
         rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
@@ -38,6 +60,7 @@ public class JumpScript : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            chrWalk.SetBool("jumping", false);
         }
     }
 }
