98b6e5d1270499f1358589a249e06667314d6888
jacobo <jacobo2101@gmail.com>
2025-01-11 23:01:35 +0100

    Fixed minor issues

--- Assets/Scripts/Boss/BossBehavior.cs
+++ Assets/Scripts/Boss/BossBehavior.cs
@@ -4,8 +4,7 @@ using UnityEditor;
 using UnityEngine;
 using System;
 
-public class BossBehavior : MonoBehaviour
-{
+public class BossBehavior : MonoBehaviour {
     [SerializeField] private GameObject target;
     [SerializeField] private DemonProyectileFactory demonProyectileFactory;
     [SerializeField] private float speed;
@@ -22,123 +21,102 @@ public class BossBehavior : MonoBehaviour
     private float health;
     private float nextCastTime = 0f;
     private Animator animator;
-   
 
 
-    void Awake()
-    {
+
+    void Awake() {
         animator = GetComponentInChildren<Animator>();
         health = maxHealth;
     }
 
     private void Start() {
-        target = GameObject.Find("CharacterRoot");
+        target = GameObject.Find("Player");
         UpdateOrientation();
     }
 
 
-    void Update()
-    {
-        if (state == State.Idle && System.Math.Abs(transform.position.x - target.transform.position.x) >= castThreshold && nextCastTime <= Time.time)
-        {
+    void Update() {
+        if (state == State.Idle && System.Math.Abs(transform.position.x - target.transform.position.x) >= castThreshold && nextCastTime <= Time.time) {
             state = State.Cast;
             UpdateOrientation();
         }
-        else if (state == State.Idle && System.Math.Abs(transform.position.x - target.transform.position.x) >= smashThreshold)
-        {
+        else if (state == State.Idle && System.Math.Abs(transform.position.x - target.transform.position.x) >= smashThreshold) {
             HandleMovement();
             UpdateOrientation();
         }
-        else if (state == State.Idle)
-        {
+        else if (state == State.Idle) {
             float k = UnityEngine.Random.Range(0f, 3f);
-            if (k >= 2)
-            {
+            if (k >= 2) {
                 state = State.Melee;
             }
-            else if (k >= 1)
-            {
+            else if (k >= 1) {
                 state = State.Smash;
             }
-            else
-            {
+            else {
                 state = State.Fire;
             }
         }
-        else if (state == State.Smash)
-        {
+        else if (state == State.Smash) {
             StartCoroutine(Smash());
             state = State.Cooldown;
         }
-        else if (state == State.Melee)
-        {
+        else if (state == State.Melee) {
             StartCoroutine(Melee());
             state = State.Cooldown;
         }
-        else if (state == State.Fire)
-        {
+        else if (state == State.Fire) {
             StartCoroutine(Fire());
             state = State.Cooldown;
         }
-        else if (state == State.Cast)
-        {
+        else if (state == State.Cast) {
             StartCoroutine(Cast());
             nextCastTime = Time.time + castTime;
             state = State.Cooldown;
-        } 
+        }
     }
 
-    public void OnHit(Collider2D other)
-    {
-        if (state == State.Idle && other.CompareTag("Bullet"))
-        {
+    public void OnHit(Collider2D other) {
+        if (state == State.Idle && other.CompareTag("Bullet")) {
             StartCoroutine(Hurt());
         }
     }
 
-    private void HandleMovement()
-    {
+    private void HandleMovement() {
         float newX = transform.position.x - orientation * speed * Time.deltaTime;
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 
-    private void UpdateOrientation()
-    {
+    private void UpdateOrientation() {
         orientation = System.Math.Sign(transform.position.x - target.transform.position.x);
         Vector3 scale = transform.localScale;
         scale.x = orientation;
         transform.localScale = scale;
     }
 
-    private IEnumerator Melee()
-    {
+    private IEnumerator Melee() {
         animator.SetTrigger("MeleeTrigger");
         yield return new WaitForSeconds(2.27f);
         state = State.Idle;
     }
 
-    private IEnumerator Smash()
-    {
+    private IEnumerator Smash() {
         animator.SetTrigger("SmashTrigger");
         yield return new WaitForSeconds(2.27f);
         state = State.Idle;
     }
 
-    private IEnumerator Fire()
-    {
+    private IEnumerator Fire() {
         animator.SetTrigger("FireTrigger");
         yield return new WaitForSeconds(2.5f);
         state = State.Idle;
     }
 
-    private IEnumerator Cast()
-    {
+    private IEnumerator Cast() {
         animator.SetTrigger("CastTrigger");
         yield return new WaitForSeconds(0.5f); //Half animation
-        foreach (float y in new float[] { 0.5f, 1.5f, 4f, 5f}) 
-        {
-            float height = UnityEngine.Random.Range(-0.2f, 3f); 
-            Vector3 spawnPoint = new(transform.position.x - projectileOffset*orientation, transform.position.y + height, transform.position.z);
+        foreach (float y in new float[] { 0.5f, 1.5f, 4f, 5f }) {
+            float height = UnityEngine.Random.Range(-0.2f, 3f);
+            Vector3 spawnPoint = new(transform.position.x - projectileOffset * orientation, transform.position.y + height, transform.position.z);
             GameObject projectile = demonProyectileFactory.CreateDemonProyectile(spawnPoint);
             DemonProyectileBehavior demonProyectileBehavior = projectile.GetComponent<DemonProyectileBehavior>();
             demonProyectileBehavior.orientation = orientation;
@@ -148,18 +126,15 @@ public class BossBehavior : MonoBehaviour
         state = State.Idle;
     }
 
-    private IEnumerator Hurt()
-    {
+    private IEnumerator Hurt() {
         health -= 1;
         state = State.Hurt;
         animator.SetTrigger("HurtTrigger");
         yield return new WaitForSeconds(0.4667f);
-        if (health > 0)
-        {
+        if (health > 0) {
             state = State.Idle;
         }
-        else
-        {
+        else {
             state = State.Dead;
             animator.SetTrigger("DeathTrigger");
             yield return new WaitForSeconds(3.2f);
--- Assets/Scripts/Boss/BossCollision.cs
+++ Assets/Scripts/Boss/BossCollision.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossCollision : MonoBehaviour
-{
+public class BossCollision : MonoBehaviour {
     private BossBehavior parentScript; // Reference to the parent script
 
-    private void Start()
-    {
+    private void Start() {
         parentScript = GetComponentInParent<BossBehavior>();
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
+    private void OnTriggerEnter2D(Collider2D other) {
         parentScript.OnHit(other);
     }
 }
