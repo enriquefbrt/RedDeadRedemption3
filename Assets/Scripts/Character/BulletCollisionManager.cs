e2e214f37c5c618473b3bf53cd519ff8ebb4d0b8
enriquefbrt <enriquefbrt@gmail.com>
2025-01-14 01:55:24 +0100

    enemies now have 3hp and magic bullet mechanic is smoother

--- Assets/Scripts/Character/BulletCollisionManager.cs
+++ Assets/Scripts/Character/BulletCollisionManager.cs
@@ -7,11 +7,13 @@ using System;
 public class BulletCollisionManager : MonoBehaviour
 {
     [SerializeField] private bool isBulletMagic;
+    private Rigidbody2D rb;
     private GameObject characterRoot;
 
     void Start()
     {
         characterRoot = GameObject.FindWithTag("Player");
+        rb = characterRoot.GetComponent<Rigidbody2D>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -23,6 +25,7 @@ public class BulletCollisionManager : MonoBehaviour
                 Vector2 impactPoint = other.ClosestPoint(transform.position);
                 int impactOrientation = Math.Sign(impactPoint.x - characterRoot.transform.position.x);
                 characterRoot.transform.position = new Vector3(impactPoint.x - 1 * impactOrientation, impactPoint.y, 0f);
+                rb.velocity = new Vector3(0f, 0f, 0f);
             }
             Destroy(gameObject);
         } 
